832b91a017f15f993864dacd1602140f361dad07
RhafAlamri <118050497+RhafAlamri@users.noreply.github.com>
2025-09-18 21:14:14 +0300

    Add ActionHelper utility class and update TestBase to integrate logging and action methods

--- src/test/java/utilities/TestBaseWithActionHelper.java
+++ src/test/java/utilities/TestBaseWithActionHelper.java
@@ -1,6 +1,8 @@
 package utilities;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.OutputType;
@@ -15,15 +17,48 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+/*
+TestBase Class:
+We create this class to avoid repeating the same setup and teardown code
+in every test class. It uses Java’s OOP (Object-Oriented Programming) concept
+of inheritance.
 
-public abstract class TestBase {
+Key points:
 
-    protected WebDriver driver;
+1. Reusability:
+   - Any class that extends TestBase automatically gets access to the driver,
+     setup(), and teardown() methods.
+   - Child classes don’t need to write these methods again.
+
+2. Access Modifier:
+   - TestBase is usually placed in a separate package.
+   - To allow child classes to use the driver, it must be declared
+     as public or protected.
+   - We choose "protected" since only child classes need it, not external classes.
+
+3. Abstract Class:
+   - We don’t want anyone to create objects of TestBase directly.
+   - To prevent this, we declare TestBase as abstract.
+   - Abstract classes can’t be instantiated; they can only be inherited.
+
+4. Usage:
+   - We will extend TestBase in our test classes.
+   - The driver variable and common methods are inherited,
+     which keeps our code shorter, cleaner, and easier to maintain.
+*/
+
+public abstract class TestBaseWithActionHelper {
+    protected  WebDriver driver;
+    protected Logger logger;
+    protected ActionHelper actionHelper;
     @BeforeEach
     void setUp() {
+        logger = LogManager.getLogger(TestBaseWithActionHelper.class);
         driver= new ChromeDriver();
+        logger.info("Chrome browser is initialized ...");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        actionHelper= new ActionHelper(driver);
     }
 
     @AfterEach
@@ -59,4 +94,4 @@ public abstract class TestBase {
         }
 
     }
-    }
+}
